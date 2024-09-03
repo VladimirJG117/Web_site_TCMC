3d60a1528b0fb4346d8dc05cff7f05868f55148c
VladimirJG117 <vladimirjuradogilipn117@gmail.com>
2024-09-03 13:53:29 -0600

    change 03/09_14:00hrs

--- js/scripts.js
+++ js/scripts.js
@@ -2,32 +2,3 @@ function toggleMenu() {
     const menu = document.getElementById('menu');
     menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
 }
-
-
-/*
-// Verificar si existe el botón de cierre del popup antes de agregar el event listener
-const closePopupButton = document.getElementById('closePopup');
-if (closePopupButton) {
-    closePopupButton.addEventListener('submit', function() {
-        // Limpiar el formulario
-        if (customForm) {
-            customForm.reset();
-        }
-
-        // Ocultar el popup
-        document.getElementById('popup').style.display = 'none';
-
-        // Redirigir a la página de inicio después de 2 segundos
-        setTimeout(function() {
-            window.location.href = 'index.html';
-        }, 2000);
-    });
-}
-
-// Validar el email
-function validateEmail(email) {
-    var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(email);
-}
-    */
-
